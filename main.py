01d63eba625380c0d3cbd3928b74167e66a058dd
ANDRE LUIZ FRANCA BATISTA <01411769627>
2022-10-24 08:49:33 -0300

    testing

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # return jsonify({"Choo Choo": "Welcome to your Flask app"})
-    return render_template("./static/index.html")
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
