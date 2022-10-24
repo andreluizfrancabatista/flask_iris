6f54bc380bebcacf58cd2d544e6e61ea45f54ca0
ANDRE LUIZ FRANCA BATISTA <01411769627>
2022-10-24 09:15:15 -0300

    testing

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ def index():
     # return jsonify({"Choo Choo": "Welcome to your Flask app"})
     return render_template("index.html")
 
+@app.route('/predict')
+def prediction():
+    if request.method == 'POST':
+        return request.form.get('val1')
+    else:
+        return {"msg": "use o form"}
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
